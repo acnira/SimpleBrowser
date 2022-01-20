0581c465bfe3c651a335814bfadbb1c27cfa21f7
acnira <jkjk19990831@gmail.com>
2022-01-21 07:49:39 +0800

    added navigation bar

--- App.js
+++ App.js
@@ -1,18 +1,121 @@
 import React, {Component} from 'react';
+import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import {WebView} from 'react-native-webview';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    // 產生一個可以儲存 textInput DOM element 的 ref
+    this.WEBVIEW = React.createRef();
+    this.state=({
+      homeUrl: 'https://github.com/facebook/react-native',
+      currentUrl: 'https://github.com/facebook/react-native',
+      canGoBack: false,
+      canGoForward: false
+    })
+  }
+
+  onNavigationStateChange(navState) {
+    this.setState({
+      currentUrl: navState.uri,
+      canGoBack: navState.canGoBack,
+      canGoForward: navState.canGoForward
+    });
+  }
+
+  onBack() {
+    this.WEBVIEW.current.goBack();
+  }
+
+  onForward() {
+    this.WEBVIEW.current.goForward();
+  }
+
+  onReload() {
+    this.WEBVIEW.current.reload();
+  }
+
+  onHomePressed() {
+    this.setState({
+      currentUrl: this.state.homeUrl
+    });
+    this.onReload();
+  }
+
   render() {
+    const homeUrl=this.state.homeUrl;
     return (
-      <WebView
-        source={{uri: 'https://github.com/facebook/react-native'}}
-        style={{marginTop: 20}}
-        onError={e => {
-          return console.log(e);
-        }}
-      />
+      <View style={{ flex: 1 }}>
+        <WebView
+          ref={this.WEBVIEW}
+          source={{uri: homeUrl}}
+          style={styles.webview}
+          onNavigationStateChange={this.onNavigationStateChange.bind(this)}
+          onError={e => {
+            return console.log(e);
+          }}
+        />
+        <View style={styles.topbar} >
+          <View style={styles.navBtn}>
+            <TouchableOpacity
+              disabled={!this.state.canGoBack}
+              onPress={this.onBack.bind(this)}
+              >
+              <Text style={this.state.canGoBack?styles.topbarText:styles.topbarTextDisabled}>Go Back</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.navBtn}>
+            <TouchableOpacity
+              disabled={!this.state.canGoForward}
+              onPress={this.onForward.bind(this)}
+              >
+              <Text style={this.state.canGoForward?styles.topbarText:styles.topbarTextDisabled}>Go Forward</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.navBtn}>
+            <TouchableOpacity
+              onPress={this.onHomePressed.bind(this)}
+              >
+              <Text style={styles.topbarText}>Home</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.navBtn}>
+          <TouchableOpacity
+            onPress={this.onReload.bind(this)}
+            >
+            <Text style={styles.topbarText}>Reload</Text>
+          </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+      
     );
   }
 }
+
+const styles = StyleSheet.create({
+  webview:{
+
+  },
+  topbar:{
+    borderColor: "#959595",
+    borderWidth: 2,
+    flexDirection:'row', 
+    alignItems:'center', 
+    justifyContent:'center'
+  },
+  navBtn:{
+    flex: 1
+  },
+  topbarText:{
+    fontSize: 20,
+    color: "#2e79bf"
+  },
+  topbarTextDisabled:{
+    fontSize: 20,
+    color: "#454545"
+  }
+});
 
 export default App;
