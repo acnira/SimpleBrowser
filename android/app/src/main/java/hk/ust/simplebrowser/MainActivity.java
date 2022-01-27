e58f5c95ffecbd9cc75a015dda6374a645f18100
acnira <jkjk19990831@gmail.com>
2022-01-27 12:18:36 +0800

    changed package name to hk.ust.simplebrowser

--- android/app/src/main/java/hk/ust/simplebrowser/MainActivity.java
+++ android/app/src/main/java/hk/ust/simplebrowser/MainActivity.java
@@ -1,11 +1,12 @@
-package com.simplebrowser;
+package hk.ust.simplebrowser;
 
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
